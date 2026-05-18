50e6644bf7b1d415ac3b8c9a0fb4d97030afb815
Eduardo Heredia <contato@eduardoheredia.net.br>
2026-05-18 18:04:11 -0300

    feat(cli): adicionar suporte à flag -h à cli

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,35 @@
 const std = @import("std");
 
+const cli = @import("args.zig");
+
 const token = @import("token.zig");
 const ast = @import("ast.zig");
 const parser = @import("parser.zig");
 const eval = @import("eval.zig");
 
 pub fn main(init: std.process.Init) !void {
-    _ = init;
+    const gpa = init.gpa;
+
+    const args = try init.minimal.args.toSlice(gpa);
+
+    defer gpa.free(args);
+    const config = try cli.configParser(args);
+
+    if (config.help) {
+        showHelp();
+        std.process.exit(0);
+    }
+}
+
+pub fn showHelp() void {
+    const msg =
+        \\Uso: comp [opções] <arquivo>
+        \\
+        \\Opções:
+        \\  -h, --help     Mostra esta ajuda
+        \\
+    ;
+    std.debug.print("{s}\n", .{msg});
 }
 
 test "importações" {
