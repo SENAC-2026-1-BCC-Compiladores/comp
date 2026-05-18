5cbcef84f216ca3a20fab70bf01ddc5fe58050c5
Eduardo Heredia <contato@eduardoheredia.net.br>
2026-05-18 17:08:30 -0300

    feat(parser): inicializar layout do projeto com módulos vazios

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,19 @@
 const std = @import("std");
 
+const token = @import("token.zig");
+const ast = @import("ast.zig");
+const parser = @import("parser.zig");
+const eval = @import("eval.zig");
+
 pub fn main(init: std.process.Init) !void {
     _ = init;
+}
+
+test "importações" {
+    std.testing.refAllDecls(@This());
+
+    _ = token;
+    _ = ast;
+    _ = parser;
+    _ = eval;
 }
